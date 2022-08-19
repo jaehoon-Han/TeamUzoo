366a8aafcb875a534379627032deff1ad773d754
jaehoon-Han <eroel1060@daum.net>
2022-08-19 17:45:28 +0900

    미완성이지만 올리기 !

--- app/src/main/java/com/han/teamuzoo/MainActivity.java
+++ app/src/main/java/com/han/teamuzoo/MainActivity.java
@@ -8,17 +8,29 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.navigation.NavigationView;
 
+//import me.tankery.lib.circularseekbar.CircularSeekBar;
+// todo: 서큘러 식바 완성하고 > Drawer Lable 고치고, 하단 메뉴바 나오게 구성하기.
 public class MainActivity extends AppCompatActivity {
 
+//    private CircularSeekBar progressCircular;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    Button buttonShow;
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -34,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigationView);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_open, R.string.menu_close);
@@ -42,9 +55,13 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+
 //                  액션바 없애는 코드 !!
 //        if (getSupportActionBar() != null) {
 //            getSupportActionBar().hide();}
+//  todo : 상단 액션바 없애고 3줄메뉴버튼 생성해서 연결하기 !!
+
+
 
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -87,6 +104,32 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+
+        buttonShow = findViewById(R.id.buttonShow);
+        buttonShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        MainActivity.this, R.style.bottomSheetDialogTheme
+                );
+                View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                        .inflate(
+                                R.layout.layout_button_sheet,
+                                (LinearLayout)findViewById(R.id.bottomSheetContainer)
+                        );
+                bottomSheetView.findViewById(R.id.buttonStart).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Start...",Toast.LENGTH_LONG).show();
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
+
 
 
 
